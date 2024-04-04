0f02be7b287f5fd3cc63870fe8fc41978dc62c56
VENKADESHKUMAR-VK <101609376+VENKADESHKUMAR-VK@users.noreply.github.com>
2024-04-04 17:47:37 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,12 @@
-
+'''
+    Author     : VENKADESH KUMAR K
+    Title      : Keylogger with Python Tkinter
+    College    : Jayaraj Annapackiam CSI College Of Engineering
+    Department : Computer Science And Engineering
+    Roll No    : 951321104053
+    Year & Sem : IIIrd and 6th Sem
+    Tools Used : tkinter, json, pynput.
+'''
 import tkinter as tk
 from tkinter import *
 from pynput import keyboard
